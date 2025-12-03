58bdad93c5fdad4fd62bc556cb370ecebb2812a4
Megan <megan.wood.cs@gmail.com>
2025-12-03 01:02:44 -0800

    1,2, and 3 month average added on analytics dashboard

--- app/analytics-dashboard/page.tsx
+++ app/analytics-dashboard/page.tsx
@@ -25,8 +25,36 @@ export default function AnalyticsDashboard() {
 
   console.log("entries on analytics: ", entries);
 
-  function calculateThreeMonthFastingAvg() {
+  function calculateFastingAvg(months: number): number {
+    let avg = 0; 
+    const values = getFastingEntriesInRange(months);
+    console.log("values: ", values); 
+    avg = computeAvg(values); 
+    return avg; 
+  }
 
+  function getFastingEntriesInRange(months: number) {
+    const fastingEntries = entries.filter((entry) => entry.type === "fasting"); 
+    console.log("fasting entries: ", fastingEntries); 
+    const curDate = new Date();
+    const thirtyDaysAgo = new Date(curDate.getTime() - (months * 30) * 24 * 60 * 60 * 1000);  // uses 30 days per month * 24 hours
+
+    const values = fastingEntries
+      .filter((entry) => {
+        const entryDate = new Date(entry.time); 
+        return entryDate >= thirtyDaysAgo && entryDate <= curDate; 
+      })
+      .map(entry => entry.glucose_value); 
+
+    return values;
+  }
+
+  function computeAvg(values: number[]) {
+    let sum = 0; 
+    for (let i = 0; i < values.length; ++i) {
+      sum += values[i];
+    }
+    return sum / values.length;
   }
 
 
@@ -35,7 +63,10 @@ export default function AnalyticsDashboard() {
     <div className="flex flex-col w-full m-5 space-y-2">
       <Link href="/protected"><Button variant="outline">Go back to dashboard homepage</Button></Link>
       <h1 className="font-bold">Your analytics:</h1>
-
+      <h2>One month fasting average: {calculateFastingAvg(1)}</h2>
+      <h2>Two month fasting average: {calculateFastingAvg(2)}</h2>
+      <h2>Three month fasting average: {calculateFastingAvg(3)}</h2>
+      
     </div>
     </>
   )
