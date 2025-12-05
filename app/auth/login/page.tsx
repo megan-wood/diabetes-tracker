0439a29a00a236abf0cc693737c00ca9a2914433
Megan <megan.wood.cs@gmail.com>
2025-12-04 19:19:56 -0800

    changed login page to redirect to protected page if user is already logged in

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,18 @@
 import { LoginForm } from "@/components/login-form";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+
+export default async function Page() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getClaims();
+  console.log("data: ", data, " error: ", error);
+  // redirect to home page if they are already logged in 
+  if (!error && data?.claims) {
+    console.log("logged in, redirecting :)")
+    redirect("/protected");
+  }
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
