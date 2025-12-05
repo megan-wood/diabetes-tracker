3ee5f778437b1f4b3ab40922ddab712b515c772e
Megan <megan.wood.cs@gmail.com>
2025-12-04 19:29:10 -0800

    changed title of webpage to Diabetes Tracker

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Diabetes Tracker",
+  description: "Diabetes tracker built with Next.js and Supabase",
 };
 
 const geistSans = Geist({
